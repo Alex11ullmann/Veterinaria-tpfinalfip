2e773395ca16af8951899c7cdfd6f5db5fedea25
NadiaMujica <nadiamuti@gmail.com>
2024-11-13 20:33:25 -0300

    Agrego clase Paciente y modificaciones en Cliente

--- Proveedor.ts
+++ Proveedor.ts
@@ -2,13 +2,13 @@ import { Red } from "./Red";
 import { Sucursal } from "./Sucursal";
 
 export class Proveedor {
-    protected nombre : string;
-    protected id : string;
-    protected telefono : number;
+    protected nombreProveedor : string;
+    protected idProveedor : string;
+    protected telefonoProveedor : number;
 
-    constructor (nombre : string, id : string, telefono : number) {
-        this.nombre = nombre;
-        this.id = id;
-        this.telefono = telefono;
+    constructor (nombreProveedor : string, idProveedor : string, telefonoProveedor : number) {
+        this.nombreProveedor = nombreProveedor;
+        this.idProveedor = idProveedor;
+        this.telefonoProveedor = telefonoProveedor;
     }
 }
